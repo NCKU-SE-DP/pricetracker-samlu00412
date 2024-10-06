93afc26741f6c780384af1e8f4646169ce61c3bf
LAPTOP-9PRMHLIN\samlu <samlu00412@gmail.com>
2024-10-06 16:03:20 +0800

    feat:新增行動版頁面漢堡選單

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,14 +1,22 @@
 <template>
     <nav class="navbar">
         <div class="title"> <RouterLink to="/overview">價格追蹤小幫手</RouterLink></div>
-        <ul class="options">
+        <ul class="options" v-if="show||widescreen">
+            <div class="divider"></div>
             <li><RouterLink to="/overview">物價概覽</RouterLink></li>
+            <div class="divider"></div>
             <li><RouterLink to="/trending">物價趨勢</RouterLink></li>
+            <div class="divider"></div>
             <li><RouterLink to="/news">相關新聞</RouterLink></li>
+            <div class="divider"></div>
             <li v-if="!isLoggedIn"><RouterLink to="/login">登入</RouterLink></li>
             <li v-else @click="logout">Hi, {{getUserName}}! 登出</li>
         </ul>
+
+        <button class="hamburger" @click="hide()">&#9776;</button>
     </nav>
+    
+    
 </template>
 
 <script>
@@ -16,6 +24,12 @@ import { useAuthStore } from '@/stores/auth';
 
 export default {
     name: 'NavBar',
+    data() {
+        return {
+            show: false,
+            widescreen: window.innerWidth > 768,
+        }
+    },
     computed: {
         isLoggedIn(){
             const userStore = useAuthStore();
@@ -30,7 +44,22 @@ export default {
         logout(){
             const userStore = useAuthStore();
             userStore.logout();
-        }
+        },
+        hide(){
+            this.show = !this.show
+        },
+        handleResize() {
+            this.widescreen = window.innerWidth > 768;
+            if (this.widescreen) {
+                this.show = false; // 在寬螢幕下隱藏漢堡選單
+            }
+        },
+    },
+    mounted() {
+        window.addEventListener('resize', this.handleResize);
+    },
+    beforeUnmount() {
+        window.removeEventListener('resize', this.handleResize);
     }
 };
 </script>
@@ -47,6 +76,9 @@ export default {
     box-shadow: 0 0 5px #000000;
 }
 
+.divider{
+    display: none;
+}
 .navbar ul {
     list-style: none;
     display: flex;
@@ -75,4 +107,40 @@ export default {
     color: #575B5D;
 }
 
+.hamburger {
+    display: none;
+    font-size: 1.5rem;
+    cursor: pointer;
+    position: absolute;
+    top: 1rem;
+    right: 1rem; /* 將按鈕移至右上角 */
+}
+
+@media (max-width: 768px) {
+
+    .divider {
+        display: block;
+        border: .2em solid #f3f3f3;
+        width: 100%; /* 控制分隔線的寬度 */
+        border-top: 1px solid rgba(175, 175, 175, 0.5); /* 半透明灰色線 */
+    }
+    .navbar ul {
+        position: fixed;
+        top: 72px;
+        left: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+        background-color: #f3f3f3;
+    }
+
+    .navbar li {
+        font-size: 1.1em;
+    }
+
+    .hamburger {
+        display: block;
+    }
+}
 </style>
