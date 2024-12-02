dba6720404055464478fca962b3ab0841c89db41
LAPTOP-9PRMHLIN\samlu <samlu00412@gmail.com>
2024-12-02 17:43:10 +0800

    chore:補充上個commit資訊
    分割get_new跟summarize function,提高可讀性

--- backend/src/main.py
+++ backend/src/main.py
@@ -30,7 +30,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+"""
+get_new should change into simple factory pattern. (not yet)
 
+the simple factory pattern as the following comment code.
+"""
 @app.on_event("startup")
 def start_scheduler():
     database = SessionLocal()
@@ -43,3 +47,35 @@ def start_scheduler():
 @app.on_event("shutdown")
 def shutdown_scheduler():
     Background_scheduler.shutdown()
+
+# def generate_summary(content):
+#     m = [
+#         {
+#             "role": "system",
+#             "content": "你是一個新聞摘要生成機器人，請統整新聞中提及的影響及主要原因 (影響、原因各50個字，請以json格式回答 {'影響': '...', '原因': '...'})",
+#         },
+#         {"role": "user", "content": f"{content}"},
+#     ]
+#
+#     completion = OpenAI(api_key="xxx").chat.completions.create(
+#         model="gpt-3.5-turbo",
+#         messages=m,
+#     )
+#     return completion.choices[0].message.content
+
+#
+# def extract_search_keywords(content):
+#     m = [
+#         {
+#             "role": "system",
+#             "content": "你是一個關鍵字提取機器人，用戶將會輸入一段文字，表示其希望看見的新聞內容，請提取出用戶希望看見的關鍵字，請截取最重要的關鍵字即可，避免出現「新聞」、「資訊」等混淆搜尋引擎的字詞。(僅須回答關鍵字，若有多個關鍵字，請以空格分隔)",
+#         },
+#         {"role": "user", "content": f"{content}"},
+#     ]
+#
+#     completion = OpenAI(api_key="xxx").chat.completions.create(
+#         model="gpt-3.5-turbo",
+#         messages=m,
+#     )
+#     return completion.choices[0].message.content
+
